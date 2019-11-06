d58c049646bdea3aa28fefdf02b333e1577c87e4
Carmel Levy <Carmel.Levy@emc.com>
2019-11-06 11:25:49 +0100

    add prediction percentage to view

--- src/app/image-classifier-webcam-client/image-classifier-webcam-client.component.ts
+++ src/app/image-classifier-webcam-client/image-classifier-webcam-client.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-image-classifier-webcam-client',
   templateUrl: './image-classifier-webcam-client.component.html',
   styleUrls: ['./image-classifier-webcam-client.component.scss']
 })
-export class ImageClassifierWebcamClientComponent implements OnInit {
+export class ImageClassifierWebcamClientComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('video') video: ElementRef;
+  @ViewChild('canvas') canvas: ElementRef;
+  label: string;
+  percentage: number;
+  constructor(private httpClient: HttpClient) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    setInterval(async () => {
+      this.canvas.nativeElement.getContext('2d').drawImage(this.video.nativeElement, 0, 0, 300, 300);
+      const imageSrc = this.canvas.nativeElement.toDataURL('image/jpeg')
+      const imageEncoded = imageSrc.replace('data:image/jpeg;base64,', '');
+      this.httpClient.post('http://127.0.0.1:9050/predict',
+        {
+          image: imageEncoded
+        })
+        .subscribe(
+            response => {
+              this.label = response['label'];
+              this.percentage = parseFloat(response['percentage']);
+            });
+    }, 1000);
+  }
+
+  async ngAfterViewInit() {
+    const vid = this.video.nativeElement;
+
+    if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then((stream) => {
+          vid.srcObject = stream;
+
+        })
+        .catch((err0r) => {
+          console.log('Something went wrong!');
+        });
+    }
   }
 
 }
